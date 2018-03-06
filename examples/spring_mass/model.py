55ebb429f3aad57abb728d449b308848e33e3bce
jwarner308 <james.e.warner@nasa.gov>
2018-03-06 11:16:21 -0500

    Updated the spring-mass example problem. Started a jupyter notebook for it

--- examples/spring_mass/model.py
+++ examples/spring_mass/model.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-
 #------------------------------------------------------
 #Helper function to use scipy integrator in model class 
 def mass_spring(state, t, k, m):
--- examples/spring_mass/spring_mass_1D.py
+++ examples/spring_mass/spring_mass_1D.py
@@ -1,20 +1,17 @@
-
 import numpy as np
-import matplotlib.pyplot as plt
 
-from model import SpringMass_1D, SpringMass_2D
+from model import SpringMass_1D
 from postprocess import Postprocessor
-from srom import SROM
-from target import BetaRandomVariable as beta
-from target import SampleRV 
+from srom import SROM, FiniteDifference as FD, SROMSurrogate
+from target import SampleRV, BetaRandomVariable 
+
+#Random variable for spring stiffness
+stiffness_rv = BetaRandomVariable(alpha=3., beta=2., shift=1., scale=2.5)
 
 #Specify spring-mass system:
 m = 1.5                             #deterministic mass
 state0 = [0., 0.]                   #initial conditions
 t_grid = np.arange(0., 10., 0.1)    #time 
-
-#random variable for spring stiffness
-stiffness_rv = beta(alpha=3., beta=2., shift=1., scale=2.5)
 
 #Initialize model
 model = SpringMass_1D(m, state0, t_grid)
@@ -38,24 +35,47 @@ mc_solution = SampleRV(disp_samples)
 #generate SROM for random stiffness
 sromsize = 10
 dim = 1
+input_srom = SROM(sromsize, dim)
+input_srom.optimize(stiffness_rv)
 
-srom = SROM(sromsize, dim)
-srom.optimize(stiffness_rv)
-(samples, probs) = srom.get_params()
+#Compare SROM vs target stiffness distribution:
+pp_input = Postprocessor(input_srom, stiffness_rv)
+pp_input.compare_CDFs()
 
 #Run model to get max disp for each SROM stiffness sample
 srom_disps = np.zeros(sromsize)
+(samples, probs) = input_srom.get_params()
 for i, stiff in enumerate(samples):
     srom_disps[i] = model.get_max_disp(stiff)
  
-#Form new SROM for the max displacement solution using samples from the model   
-srom_solution = SROM(sromsize, dim)
-srom_solution.set_params(srom_disps, probs)
-
-#----------------------------------------
+#Form new SROM for the max disp. solution using samples from the model   
+output_srom = SROM(sromsize, dim)
+output_srom.set_params(srom_disps, probs)
 
 #Compare solutions
-pp = Postprocessor(srom_solution, mc_solution)
-pp.compare_CDFs()
+pp_output = Postprocessor(output_srom, mc_solution)
+pp_output.compare_CDFs()
+
+#--------Piecewise LINEAR surrogate with gradient info-------
+
+#Need to calculate gradient of output wrt input samples first 
+
+#Perturbation size for finite difference
+stepsize = 1e-12
+samples_fd = FD.get_perturbed_samples(samples, perturb_vals=[stepsize])
+
+#Run model to get perturbed outputs for FD calc.
+perturbed_disps = np.zeros(sromsize)
+for i, stiff in enumerate(samples_fd):
+    perturbed_disps[i] = model.get_max_disp(stiff)
+gradient = FD.compute_gradient(srom_disps, perturbed_disps, [stepsize])
+
+surrogate_PWL = SROMSurrogate(input_srom, srom_disps, gradient)
+stiffness_samples = stiffness_rv.draw_random_sample(num_samples)
+output_samples = surrogate_PWL.sample(stiffness_samples)
+solution_PWL = SampleRV(output_samples)
+
+pp_pwl = Postprocessor(solution_PWL, mc_solution)
+pp_pwl.compare_CDFs()
 
 
