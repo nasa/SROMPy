935fd950befc199e6708f2bdc43350e2f949f042
jwarner308 <james.e.warner@nasa.gov>
2018-03-26 11:11:54 -0400

    Added gamma random variable to targets

--- target/__init__.py
+++ target/__init__.py
@@ -1,6 +1,7 @@
 
 from AnalyticRV import AnalyticRV
 from BetaRandomVariable import BetaRandomVariable
+from GammaRandomVariable import GammaRandomVariable
 from NormalRandomVariable import NormalRandomVariable
 from RandomVector import RandomVector
 from SampleRV import SampleRV
