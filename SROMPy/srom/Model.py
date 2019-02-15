b7defcccc78c2f90eac6e00d03bf37b4b721899a
Juan Barrientos <juan.m.barrientos@nasa.gov>
2019-02-15 08:26:06 -0500

    Added docstring to Model class

--- SROMPy/srom/Model.py
+++ SROMPy/srom/Model.py
@@ -1,7 +1,14 @@
 import abc
 
 class Model(object):
+    """
+    Abstract base class for Models which should evaluate sample inputs to
+    produce outputs.
+
+    :param inputs: one dimensional ndarray
+    :return: two dimensional ndarray
     
+    """
     @abc.abstractmethod
     def evaluate(self, inputs):
         raise NotImplementedError
